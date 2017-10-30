45831738e52d48d56a510afd09d1cfeff778f202
Anna Khmelyuk <anna@khmelyuk.com>
2017-10-31 00:30:59 +0100

    must do right

--- js/nb1.js
+++ js/nb1.js
@@ -1,9 +1,21 @@
-function plus() {
+function myFunction() {
 var num1, num2, result;
  	num1 = document.getElementById("n1").value;
  	num1 = parseInt(num1);
 	num2 = document.getElementById("n2").value;
 	num2 = parseInt(num2);
-	result = num1 + num2 ; 
+	if (((num1 % 2) == 0), ((num2%2) == 0)) {
+		result = ("both odd");
+	}
+	else if (((num1 % 2) == 0), ((num2%2) != 0)) {
+		result = ("first odd,second even");
+	} 
+	else if (((num1 % 2) != 0), ((num2%2) == 0)) {
+		result = ("first even,second odd");
+	}
+	else if (((num1 % 2) != 0), ((num2%2) != 0)) {
+		result = ("both even")
+	}
+
 	document.getElementById("out").innerHTML = result;
 }
